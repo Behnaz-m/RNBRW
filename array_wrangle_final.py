8865c686b3129019e791a36a90aaca4593d16c32
Behnaz-m <58396350+Behnaz-m@users.noreply.github.com>
2021-01-04 16:55:55 -0500

    Update array_wrangle_final.py

--- array_wrangle_final.py
+++ array_wrangle_final.py
@@ -1,10 +1,10 @@
 import numpy as np
 import sys
 m = 150580575 
-T = 9
-address = '/sfs/qumulo/qhome/bm7mp/OS/rnbrw/output/git_full_total'
+T = 10
+address = '/Path/git_full_total'
 #arg = int(sys.argv[1])# arge is counter for output
-arg = 2 #for the second time when we only have one out put
+arg = 1 #for the second time when we only have one out put
 # T is the number of runs per array, say I have 1000 git out put:git_0-git_999, if T=100 this array adds every 100's runs(10ta 100 taei)and gives 10 outputs
 
 X = np.zeros(m , dtype = int )
@@ -17,5 +17,5 @@ for i in range((arg-1)*T+1, (arg)*T+1):
     except:
         continue  
         
-np.savetxt('/sfs/qumulo/qhome/bm7mp/OS/rnbrw/output/git_ful_total', X.astype(int), fmt='%i') 
+np.savetxt('/Path/git_ful_total', X.astype(int), fmt='%i') 
 
